fa2d9aa91d942a1d4f042f68d12ef79b3549cbac
kywon <kywon12200@gmail.com>
2025-08-03 21:57:35 +0900

    tmdbapi 연동

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify, render_template
 from recommender.movieRecommender import MovieRecommender
+from tmdbApi.tmdbApi import tmdbApi
 
 app = Flask(__name__)
 
-movieRecommender = MovieRecommender('ml-latest-small/movies.csv', 'ml-latest-small/ratings.csv', 'ml-latest-small/tags.csv')
+movieRecommender = MovieRecommender('ml-latest-small/movies.csv', 'ml-latest-small/ratings.csv', 'ml-latest-small/tags.csv', 'ml-latest-small/links.csv')
+tmdbApi = tmdbApi('a97e49c6c499e9985d3b1f95942b557c')
 
 @app.route("/")
 def home():
@@ -27,6 +29,11 @@ def movieTitle():
     query = request.get_json().get('query', '').lower()
     titles = movieRecommender.get_titles(query)
     return jsonify(titles)
+
+@app.route("/detail/<int:tmdbId>")
+def movieDetail(tmdbId):
+    poster = tmdbApi.get_poster_url(tmdbId)
+    return render_template("movieDetail.html", poster_path=poster)
 
 if __name__ == "__main__":
     app.run(debug=True)
